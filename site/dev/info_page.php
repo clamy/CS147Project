c7b46b7a76c1e6cef124b660e1945c1a2e0343e0
clamy <camm_lamy@hotmail.com>
2012-10-31 22:18:34 -0700

    Starting database queries on infopage

--- site/dev/info_page.php
+++ site/dev/info_page.php
@@ -1,3 +1,46 @@
+<?php
+require("../lib/database_settings.php");
+$id = $_GET["id"];
+
+function GetAssociatedArray($id, $DBH){
+	try {
+        $STH = $DBH->prepare("SELECT * FROM places WHERE id=:id");
+        $STH->bindParam(':id', $id);
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+        $STH->execute();
+        if ($row = $STH->fetch()) {
+            return $row;
+        } else {
+            return NULL;
+        }
+    } catch (PDOException $e) {
+        print $e->getMessage();
+    }
+	
+}
+
+$associated_array = GetAssociatedArray($id,$DBH);
+
+function GetPicturesArray($id,$DBH){
+	try {
+        $STH = $DBH->prepare("SELECT * FROM pictures WHERE placeid=:id");
+        $STH->bindParam(':id', $id);
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+        $STH->execute();
+        if ($row = $STH->fetchAll()) {
+            return $row;
+        } else {
+            return NULL;
+        }
+    } catch (PDOException $e) {
+        print $e->getMessage();
+    }
+}
+
+$pictures_array = GetPicturesArray($id,$DBH);
+
+?>
+
 <!DOCTYPE html> 
 <html>
 
@@ -26,21 +69,33 @@
 
 	<div data-role="header" data-theme="a">
     	<a href="list.php" data-icon="arrow-l">Back</a>
-		<h1>Hoover Tower</h1>
+		<h1><?php echo $associated_array["name"];?></h1>
         <a href="#popupHelp" data-rel="popup" data-position-to="window" data-transition="fade" data-icon="info">Help</a>
         <div data-role="navbar">
 			<ul>
-				<li><a href="#popupDirections" data-rel="popup" data-position-to="window" data-transition="fade">Directions</a></li>
 				<li><a href="#popupAdd" data-rel="popup" data-position-to="window" data-transition="fade">Add Trivia</a></li>
                 <li><a href="info_page.php" >Upvote</a></li>
+                <li><a href="info_page.php" >Downvote</a></li>
 			</ul>
 		</div><!-- /navbar -->
 	</div><!-- /header -->
 	<div data-role="content" data-theme="a">
     
+    
+    
     <ul data-role="listview">
     <li>
-    	<div data-role="collapsible"  data-collapsed="false">
+    <div class="ui-grid-a">
+		<div class="ui-block-a">
+            <a href="#popupPhoto" data-rel="popup" data-position-to="window" data-transition="fade"><img src="img/places/hoover_tower/hoover_tower.jpg" width = "90%"></a>
+       	</div>
+		<div class="ui-block-b">
+                	<a href="#popupMap" data-rel="popup" data-position-to="window" data-transition="fade"><img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo $associated_array["lat"];?>,<?php echo $associated_array["lng"];?>&amp;zoom=15&amp;size=210x149&amp;markers=<?php echo $associated_array["lat"];?>,<?php echo $associated_array["lng"];?>&amp;sensor=false" width="90%"></a>
+        </div>
+	</div><!-- /grid-a -->
+    </li>
+    <li>
+    	<div data-role="collapsible" >
    			<h3>Pictures</h3>
    			<div class="ui-grid-a">
 				<div class="ui-block-a">
@@ -117,10 +172,9 @@
     		<p>This is the help page</p>
    	</div>
     
-    <div data-role="popup" id="popupDirections" data-theme="a" class="ui-corner-all">
-    		<a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a>
-    		<p>You will get directions here</p>
-   	</div>
+    <div data-role="popup" id="popupMap" data-overlay-theme="a" data-theme="d" data-corners="false">
+			<a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a><img src="https://maps.googleapis.com/maps/api/staticmap?center=<?php echo $associated_array["lat"];?>,<?php echo $associated_array["lng"];?>&amp;zoom=15&amp;size=210x300&amp;markers=<?php echo $associated_array["lat"];?>,<?php echo $associated_array["lng"];?>&amp;sensor=false" width="90%">
+	</div>
     
     </div>
 
