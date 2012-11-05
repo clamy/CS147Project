aaa8b0cc77e22218ccd1507efd7b008daafcf026
Yacine Merouchi <merouchi@corn19.stanford.edu>
2012-11-04 20:42:08 -0800

    Add user sessions and log out feature
    I've had to do unspeakable hacks to make this work, but now you should
    be able to get username (and potentially other user information) by
    using session_start() and the $_SESSION array.

--- site/dev/index.php
+++ site/dev/index.php
@@ -1,10 +1,23 @@
 <?php
+session_start();
+if ($_POST["logout"]) {
+    echo "<!-- logging out -->";
+    $_SESSION = array();
+    session_destroy();
+}
+if ($_POST["username"] && !$_POST["logout"]) {
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["token"] = $_POST["token"];
+    die();
+}
+
 require("php/header.php");
 ?>
 
 <div data-role="page">
 	<div data-role="content" data-theme="a">
         <img width="100%" src="img/logo_loading.png" />
+        <p id="serentripity-login-message"></p>
         <a id="serentripity-explore-button" data-role="button" href="list.php">Find places around me</a>
         <form id="serentripity-login-form" action="" data-ajax="false" method="post">
             <input id="serentripity-login-username" type="text" name="username" placeholder="Username" data-mini="true" />
@@ -15,20 +28,31 @@ require("php/header.php");
             </div>
             <p id="serentripity-login-error"></p>
         </form>
-        <p id="serentripity-login-message"></p>
+        <form id="serentripity-logout-form" action="index.php" data-ajax="false" method="post">
+            <input type="hidden" name="logout" value="true"/>
+            <input type="submit" id="serentripity-logout-button" value="Logout" />
+        </form>
     </div>
 </div>
 
 <script>
-//var user_info = <?php json_encode(user_info); ?>;
-
-function fadeToLoggedInPage(login_response) {
-    $("#serentripity-login-form").fadeOut(function () {
-        $("#serentripity-login-message").fadeIn();
-    });
-    $("#serentripity-login-message").append("Logged in as " + login_response.username);
+// This function creates a message that acknowledges login.
+function fillLoggedInMessage() {
+    $("#serentripity-login-message").append("Logged in as " + session_data.username);
 }
 
+// This function makes the login form disappear and replaces it with the logout
+// button and the login message.
+function fadeToLoggedInPage() {
+    fillLoggedInMessage();
+    $("#serentripity-login-form").fadeOut(function () {
+        $("#serentripity-login-message").fadeIn();
+        $("#serentripity-logout-form").fadeIn();
+    });
+}
+
+// This function handles the response from the AJAX call that is made upon
+// submission of the login / register form.
 function handleFormResponse(response) {
     console.log(response);
     if (response.login_error) {
@@ -41,40 +65,46 @@ function handleFormResponse(response) {
         $("#serentripity-login-error").fadeIn();
     } else {
         console.log("Got good login");
-        fadeToLoggedInPage(response);
-    }
-}
-$(document).ready(function () {
-    console.log("document is ready");
-    $("#serentripity-login-form").show();
-    // We bind a function to the "submit" event for the login form.
-    $("#serentripity-login-form").submit(function (e) {
-        console.log("Login form submitted");
-        // Prevent the default behavior from firing.
-        e.preventDefault();
-        var login_data = $("#serentripity-login-form").serializeArray();
+        $.extend(session_data, response);
+        fadeToLoggedInPage();
         $.ajax({
             type: "post",
-            url: "php/login_or_register.php",
-            data: login_data,
-            dataType: "JSON",
-            success: handleFormResponse
+            data: response,
+            url: "index.php"
         });
+    }
+}
+
+// We bind a function to the "submit" event for the login form.
+$("#serentripity-login-form").submit(function (e) {
+    console.log("Login form submitted");
+    // Prevent the default behavior from firing.
+    e.preventDefault();
+    var login_data = $("#serentripity-login-form").serializeArray();
+    $.ajax({
+        type: "post",
+        url: "php/login_or_register.php",
+        data: login_data,
+        dataType: "JSON",
+        success: handleFormResponse
     });
-    /*
-    // Check if the user is logged.
-    if (user_info.wrong_login || !user_info.token) {
-        console.log("No valid username found.");
-        $("#serentripity-login-form").show();
-    } else {
-        $("#serentripity-login-message").show();
-    }
-    // If the user did an unsuccessful login attempt, display an error message.
-    if (user.info.wrong_login) {
-        $("#serentripity-login-error").show();
-    }
-    */
 });
+
+// Once the document is ready, we should display the appropriate elements
+// depending on the user's session status.
+$(document).ready(function () {
+    console.log("document is ready");
+    if (session_data.username) {
+        fillLoggedInMessage();
+        $("#serentripity-login-message").show();
+        $("#serentripity-logout-form").show();
+    } else {
+        $("#serentripity-login-form").show();
+    }
+});
+
+
+
 </script>
 
 <?php
