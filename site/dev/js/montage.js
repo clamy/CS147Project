9767678627705bd0b00a114235ebfee6d2c7f6bd
Yacine Merouchi <merouchi@corn28.stanford.edu>
2012-11-07 12:09:24 -0800

    Add multiblocks

--- site/dev/js/montage.js
+++ site/dev/js/montage.js
@@ -8,53 +8,71 @@ site.montage = function(lat,lng,target_element) {
     // We save a reference to this function.
     that = this;
 	
-    this.addThumbnail = function (width_percent, height_percent) {
+	function flipCoin() {
+		return Math.random() > 0.5 ;
+	}
+	
+	// TODO(yacinem): width_percent, height_percent don't do anything, investigate this
+	// and maybe remove them.
+    this.addThumbnail = function (width_percent, height_percent, tall) {
 		 var anchor_tag, image_tag;
 		 anchor_tag = $("<a class=\"serentripity-place-anchor\"  data-ajax=\"false\">");
-		 if (
 		 image_tag = $("<img class=\"serentripity-place-image\" width=\""
 			 + width_percent
 			 + "%\" height=\""
 			 + height_percent
 			 + "%\" />");
-		  return anchor_tag.append(image_tag);
+		 if (tall) {
+			 image_tag.addClass("serentripity-place-image-tall");
+		 } else {
+			 image_tag.addClass("serentripity-place-image-wide");
+		 }
+		 return anchor_tag.append(image_tag);
 	};
 	
 	this.getSoloBlock = function () {
 		console.log("Adding solo block to the list.");
-		var anchor_tag = this.addThumbnail("100", "100");
+		var anchor_tag = this.addThumbnail("100", "100", false);
 		var new_block = $("<div class=\"ui-grid-solo\"></div>")
-			.append($("<div class=\"ui-block-a\"></div>").append('<p class="serentripity-place-distance"></p>').append(anchor_tag));
+			.append($("<div class=\"ui-block-a\"></div>")
+			.append('<p class="serentripity-place-distance"></p>')
+			.append(anchor_tag));
 		return new_block;
 	};
-		
-	this.getMultiBlock = function (places, wide_images, tall_images, num_images) {
+
+	this.getMultiBlock = function (num_images) {
 		console.log("Adding multi block to the list.");
 		var grid, left_column, right_column_start, right_column;
 		grid = $("<div class=\"ui-grid-a\"></div>");
 		left_column = $("<div class=\"ui-block-a\"></div>");
-		right_column = "<div class=\"ui-block-b\"></div>";
-		if (num_images > 2) {
-			right_column.append(this.addThumbnail("50", "50"));
-			right_column.append(this.addThumbnail("50", "50"));
-			if (num_images > 3) {
-				left_column.append(this.addThumbnail("50", "50"));
-				left_column.append(this.addThumbnail("50", "50"));
-			} else {
-				left_column.append(this.addThumbnail("50", "100"));
-			}
-		} else {
-			left_column.append(this.addThumbnail("50", "100"));
-			right_column.append(this.addThumbnail("50", "100"));
+		right_column = $("<div class=\"ui-block-b\"></div>");
+		if (num_images == 2) {
+			left_column.append(this.addThumbnail("100", "100", true));
+			right_column.append(this.addThumbnail("100", "100", true));
+		} else if (num_images == 3) {
+			left_column.append(this.addThumbnail("100", "100", true));
+			right_column.append(this.addThumbnail("100", "50", false));
+			right_column.append(this.addThumbnail("100", "50", false));
 		}
 		return grid.append(left_column).append(right_column);
 	};
-	
-	this.generateBlocks = function (num_blocks) {
-		
-		while (num_blocks--) {
-			target_element.append( this.getSoloBlock());
-			
+
+	this.generateBlocks = function (places) {
+		console.log("Generating the blocks.");
+		var places_added = 0;
+		for (var i = 0; i < places.length; i += places_added) {
+			if (i + 2 < places.length 
+				&& (places[i].score * 0.95 <= places[i + 2].score)) {
+				places_added = 2 + flipCoin();
+			} else {
+				places_added = 1;
+			}
+			console.log("Decided to add " + places_added + " place(s).");
+			if (places_added == 1) {
+				target_element.append(this.getSoloBlock());
+			} else {
+				target_element.append(this.getMultiBlock(places_added));
+			}
 		}
 	};
 	
@@ -65,24 +83,34 @@ site.montage = function(lat,lng,target_element) {
 			$("p.serentripity-place-distance").each(function(i,obj){
 				var distance = parseFloat(places[i].distance);
 				distance = distance.toFixed(1);
-				console.log(distance);
-				$(obj).append(distance+" miles");
+				$(obj).append(distance + " miles");
 			});
 	};
 
-	this.fillPlaceImages = function (places){
-		$("img.serentripity-place-image").each(function(i,obj){
-			site.getImages(places[i].id, function( images){
+	this.fillPlaceImages = function (places) {
+		$("img.serentripity-place-image").each(function(i, obj){
+			var aspect_ratio, image_index;
+			/*
+			// Select a wide or tall image depending on the aspect ratio.
+			aspect_ratio = $(obj).width() / $(obj).height();
+			console.log("Aspect ratio " + aspect_ratio);
+		 	image_index =  aspect_ratio >= 1 ? 0 : 1;
+			*/
+			image_index = $(obj).hasClass("serentripity-place-image-tall") ? 1 : 0;
+			site.getImages(places[i].id, function(images) {
+				console.log("Fetched images: ");
 				console.log(images);
-				console.log(obj);
-				$(obj).attr("src",'img/places/'+images[0].file);
+				// TODO(all): make sure there are enough images for each monument!
+				image_index = Math.min(image_index, images.length);
+				console.log("Choosing image " + image_index);
+				$(obj).attr("src", "img/places/" + images[image_index].file);
 			});
 		});
 	};
 	
     this.buildMontage = function(places) {
         console.log("Building the montage.");
-		this.generateBlocks(places.length);
+		this.generateBlocks(places);
         this.fillPlaceInfo(places);
 		this.fillPlaceImages(places);
     };
