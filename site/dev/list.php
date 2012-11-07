96b24b1f02ec078aa17fd53b78863a25057647ac
Yacine Merouchi <merouchi@corn01.stanford.edu>
2012-11-07 15:00:10 -0800

    Fix no display bug, remove back button
    I've removed the back button for now because it doesn't work, and I've
    replaced it with a home button. I've also made the links non-ajax, and
    put the script tags inside the page block (apparently we're supposed to
    do that for jQuery mobile). This seems to have fixed the problem of
    JS-generated stuff not appearing on the pages.

--- site/dev/list.php
+++ site/dev/list.php
@@ -2,12 +2,13 @@
 // TODO(all): fix a bug where nothing appears on this page from time to time, until
 // it is refreshed. I suspect this might be because of caching.
 // - yacinem
+// Update: I think I've fixed this, let me know if you see it again - yacinem
 require("php/header.php");
 ?>
-<div data-role="page" data-add-back-btn="true">
+<div data-role="page">
 
 	<div data-role="header" data-theme="a">
-    	<a href="index.php" data-icon="arrow-l">Back</a>
+    	<a href="index.php" data-icon="arrow-l" data-ajax="false">Home</a>
 		<h1>Serentripity</h1>
         <a href="#popupHelp" data-rel="popup" data-position-to="window" data-transition="fade" data-icon="info">Help</a>
 	</div><!-- /header -->
@@ -23,33 +24,30 @@ require("php/header.php");
             <p>Troubleshooting: if you see a blank page, hit refresh and make sure you allow Serentripity to find your location.</p>
     	</div>
     </div>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script>
+    var lat ;
+    var lng ;
+    function success(position){
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        lat = latlng.lat();
+        lng = latlng.lng();
+        var montage_element;
+        montage_element =  $("#serentripity-montage");
+        site.montage(lat,lng,montage_element);
+    }
+    function error(msg) {
+        lat = 0;
+        lng = 0;
+        console.log(msg);
+    }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        error('not supported');
+    }
+    </script>
 </div>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-w<script>
-var lat ;
-var lng ;
-function success(position){
-	var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	lat = latlng.lat();
-	lng = latlng.lng();
-	var montage_element;
-	montage_element =  $("#serentripity-montage");
-	site.montage(lat,lng,montage_element);
-}
-function error(msg) {
-	lat = 0;
-	lng = 0;
-	console.log(msg);
-}
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(success, error);
-} else {
-	error('not supported');
-}
-
-</script>
-
-
 <?php
 require("php/footer.php");
 ?>
